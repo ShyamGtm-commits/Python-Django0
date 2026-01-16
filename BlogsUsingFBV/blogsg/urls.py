6a57104d8642168677a4b61fb16301794fe6b209
ShyamGtm-commits <gautamshyam595@gmail.com>
2026-01-16 13:56:02 +0545

    Made the view and create blog using function views

--- BlogsUsingFBV/blogsg/urls.py
+++ BlogsUsingFBV/blogsg/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.BlogsView)
+    path('', views.BlogsView, name='blog_list'),
+    path('blogs_create/', views.BlogsCreate, name = 'blog_create')
 ]
--- BlogsUsingFBV/blogsg/views.py
+++ BlogsUsingFBV/blogsg/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import blog
+from .forms import blogForm
 
 # Create your views here.
 
+
 def BlogsView(request):
     blogs = blog.objects.all()
-    return render(request, 'blog_list.html', {'blogs': blog})
+    return render(request, 'blog_list.html', {'blogs': blogs})
 
+
+def BlogsCreate(request):
+    if request.method == "POST":
+        form = blogForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+        
+    else:
+        form = blogForm
+    
+    return render(request, 'blog_form.html', {'form': form})
+        
